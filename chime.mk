fcd1ad9303db2fdf5eacecea6d8ae51573bdfc06
LuK1337 <priv.luk@gmail.com>
2022-11-26 23:25:31 +0100

    chime: overlay: Configure aux camera
    * idek
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- chime.mk
+++ chime.mk
@@ -308,6 +308,8 @@ PRODUCT_PACKAGES += \
     TelephonyResChime \
     WifiResCommon
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
