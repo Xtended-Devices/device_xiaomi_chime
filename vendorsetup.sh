658f44d86bfc13eaac716a84a6e91a0cb1f78742
hac4us06 <hac4usanime@gmail.com>
2023-02-06 22:53:44 +0000

    Chime added vendorsetup.sh
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+# ROM source patches
+
+color="\038[0;32m"
+end="\038[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+
+# Clone kernel
+git clone https://github.com/hac4us06/kernel_xiaomi_chime -b pyro kernel/xiaomi/chime
+
+# Clone vendor
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_chime -b tithonia vendor/xiaomi/chime
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_citrus -b lineage-20 vendor/xiaomi/citrus
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_lime -b lineage-20 vendor/xiaomi/lime
+# Hardware
+git clone https://github.com/hac4us06/hardware_xiaomi-1 -b pyro hardware/xiaomi
