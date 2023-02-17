6ba89191db568cab3db021829486a5c4c15059c6
hac4us06 <hac4usanime@gmail.com>
2023-02-17 22:43:00 +0100

    update hardware trees
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 # ROM source patches
 
-color="\038[0;32m"
-end="\038[0m"
+color="\033[0;32m"
+end="\033[0m"
 
 echo -e "${color}Applying patches${end}"
 sleep 1
@@ -15,4 +15,4 @@ git clone https://gitlab.com/chimeoss/vendor_xiaomi_chime -b tithonia vendor/xia
 git clone https://gitlab.com/chimeoss/vendor_xiaomi_citrus -b lineage-20 vendor/xiaomi/citrus
 git clone https://gitlab.com/chimeoss/vendor_xiaomi_lime -b lineage-20 vendor/xiaomi/lime
 # Hardware
-git clone https://github.com/hac4us06/hardware_xiaomi-1 -b pyro hardware/xiaomi
+git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi
