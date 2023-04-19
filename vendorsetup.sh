5ff00f5a7e3e5bcfb7fc798abe53650d0165a461
hac4us06 <hac4usanime@gmail.com>
2023-04-19 18:07:27 +0000

    chime: track our fork of vendor trees
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,8 +13,8 @@ git clone https://github.com/hac4us06/kernel_xiaomi_chime -b pyro-wip kernel/xia
 (cd kernel/xiaomi/chime; git submodule init; git submodule update; sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c)
 
 # Clone vendor
-git clone https://gitlab.com/chimeoss/vendor_xiaomi_chime -b tithonia vendor/xiaomi/chime
-git clone https://gitlab.com/chimeoss/vendor_xiaomi_citrus -b lineage-20 vendor/xiaomi/citrus
-git clone https://gitlab.com/chimeoss/vendor_xiaomi_lime -b lineage-20 vendor/xiaomi/lime
+git clone https://gitlab.com/hac4us06/vendor-xiaomi-chime-13 -b lineage-20 vendor/xiaomi/chime
+git clone https://gitlab.com/hac4us06/vendor-xiaomi-citrus-13 -b lineage-20 vendor/xiaomi/citrus
+git clone https://gitlab.com/hac4us06/vendor-xiaomi-lime-13 -b lineage-20 vendor/xiaomi/lime
 # Hardware
 git clone https://github.com/hac4us06/hardware_xiaomi -b thirteen hardware/xiaomi
